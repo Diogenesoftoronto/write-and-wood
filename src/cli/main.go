9bc9650175cbdfe525c632d3f19762f3d3064618
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-08-17 01:18:38 -0400

    Update main.go

---
+++ src/cli/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Diogenesoftoronto/write-and-wood/src/cli/cmd"
+)
+
+func main() {
+	fmt.Println("Hello, writers. I brought the wood.")
+	cmd.Execute()
+}
