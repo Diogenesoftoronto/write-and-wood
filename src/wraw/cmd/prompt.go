c022e9aab1debfe9ae3727ce236f4e05ddb97ec6
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-08-17 22:53:58 -0400

    Update prompt.go
    add a description of the command

--- src/wraw/cmd/prompt.go
+++ src/wraw/cmd/prompt.go
@@ -1,6 +1,17 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package cmd
 
@@ -12,16 +23,15 @@ import (
 
 // promptCmd represents the prompt command
 var promptCmd = &cobra.Command{
-	Use:   "prompt",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "prompt [args]",
+	Short: "This command will respond with a prompt of your choice",
+	Long: `This command will help you manage getting and answering prompts.
+			To get a prompt simply use the prompt command without any arguments.
+			In order for this command to work you should be signed in with you account first. 
+			For additional information visit: 
+			https://www.github.com/Diogenesoftoronto/write-and-wood/README.md`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("prompt called")
+		fmt.Println("here is a prompt")
 	},
 }
 
@@ -32,7 +42,7 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// promptCmd.PersistentFlags().String("foo", "", "A help for foo")
+	promptCmd.PersistentFlags().StringVarP(&File, "file", "f", "", "A File name to unzip and open in IDE")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
