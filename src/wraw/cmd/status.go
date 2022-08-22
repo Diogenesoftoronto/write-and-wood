56843b1e29b198d1a942f6c5501b28fe4d7e116d
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-08-22 02:06:14 -0400

    Update status.go
    fulfill description of flags for status.

--- src/wraw/cmd/status.go
+++ src/wraw/cmd/status.go
@@ -21,7 +21,7 @@ var statusCmd = &cobra.Command{
 	This will show the amount of views you have, and other engagment stats about you and your content.
 	Understanding how other people engage with your content is very important to writers.
 	The status command gives you information about engagment but it also provides information about your writing.
-	It will tell you the number of challenges you have completed, the number of prompts created, 
+	It will tell you the number of statuss you have completed, the number of prompts created, 
 	your total word count and the rank your content has compared to others based on these metrics.
 
 	Always remember that the rank or metrics you have does not mean you are a bad or good writer, merely what we can measure.
@@ -33,10 +33,10 @@ var statusCmd = &cobra.Command{
 			Default is to display your current metrics. But this can be set with the --time/-t flag. 
 			Passing a date will display the metrics overtime between then in now.
 		- "--views/-v": This flag shows you just the view metrics. Like the engagment metrics, this can be set with the --time/-t flag. 
-		 You can also use the --prompt/-p or --challenge/-c flags to show the view metrics for a specific prompt or challenge. 
-		- "--words": This shows you just the words metrics. Like the engagment metrics, this can be set with the --time/-t flag.
+		 You can also use the --prompt/-p or --status/-c flags to show the view metrics for a specific prompt or status. 
+		- "--words/-wr": This shows you just the words metrics. Like the engagment metrics, this can be set with the --time/-t flag.
 			You can observe how many words you have written overtime to see if you are increasing or not. 
-		- "commits": This shows you how many changes or additions you have made.
+		- "commits/-c": This shows you how many changes or additions you have made.
 			Those familiar with programming the system is very similar to git. You can set the time as usual with the --time/-t flag.
 	
 	You can combine status flags to create a more statistics.
@@ -66,13 +66,10 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	challengeCmd.PersistentFlags().String("time", "ti", "The time in ")
-challengeCmd.PersistentFlags().String("words", "wr", "The words that the challenge must contain")
-	challengeCmd.PersistentFlags().String("characters", "ch", "The characters that the challenge must contain")
-	challengeCmd.PersistentFlags().String("regex", "re", "The regular expression that the challenge must match")
-	challengeCmd.PersistentFlags().String("random-complexity", "rc", "The number of random challenges to be included in the challenge")
-challengeCmd.PersistentFlags().String("private", "pr", "The challenge will be private")
-	challengeCmd.PersistentFlags().String("charcount", "cc", "The number of characters that the challenge must contain")
-
+	statusCmd.PersistentFlags().String("time", "ti", "Passing a date as a unix timestamp will display the metrics overtime between then in now.")
+	statusCmd.PersistentFlags().String("words", "wr", "Word metrics will be displayed.")
+	statusCmd.PersistentFlags().String("commits", "c", "Commit metrics will be displayed")
+	statusCmd.PersistentFlags().String("engagment", "e", "Engagment metrics will be displayed")
+	statusCmd.PersistentFlags().String("views", "v", "View metrics will be displayed")
 
 }
